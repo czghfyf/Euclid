4c352243edc1603ec1b8a747c0d19118eeddbcf7
yding7 <wzhang3@hfyf.edu>
2021-04-22 20:42:38 +0800

    ^ test

--- tests/test-0.c
+++ tests/test-0.c
@@ -1,4 +1,11 @@
 #include <stdio.h>
+ // #include <sys/types.h>
+ // #include <sys/socket.h>
+ // #include <netinet/in.h>
+ #include <arpa/inet.h>
+ // #include <unistd.h>
+ // #include <string.h>
+ #include <stdlib.h>
 
 int main(argc, argv)
 
@@ -6,6 +13,8 @@ int argc;
 char *argv[];
 {
 	fn000(1000000, 666);
+
+	printf("IPPROTO_TCP --- %d    %ld\n", IPPROTO_TCP, IPPROTO_TCP);
 }
 
 fn000(a, b)
