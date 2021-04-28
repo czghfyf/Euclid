1d2a3d42c372858f75b48bbeb607f11b4e6679c8
yding7 <hellochenzg@163.com>
2021-04-28 21:31:47 +0800

    ^ .gitignore

---
+++ tests/size_t-test.c
@@ -0,0 +1,10 @@
+#include <arpa/inet.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc,char *argv[])
+{
+	printf("%d\n", sizeof(size_t));
+	printf("%d\n", sizeof(ssize_t));
+}
