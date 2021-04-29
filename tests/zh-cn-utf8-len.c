582ad1da4125a4d01d2a41ae0a63c30f9fd1541e
yding7 <hellochenzg@163.com>
2021-04-29 14:39:30 +0800

    + test

---
+++ tests/zh-cn-utf8-len.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+
+int
+main (int argc, char **argv)
+{
+	char *usa = "USA";
+	char *china = "中国";
+
+	printf("%d\n", strlen(usa));
+	printf("%d\n", strlen(china));
+
+  return 0;
+}
