820c8171801e42eccc3c4464d4e74ada04a8ce24
yding7 <hellochenzg@163.com>
2021-04-29 14:33:48 +0800

    ^ utils

--- src/utils.c
+++ src/utils.c
@@ -1,12 +1,57 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 #include "utils.h"
 
-void print_byte_binary(char *c)
+void
+print_byte_binary (char *c)
 {
-	int offset = 7;
-	while (offset >= 0) {
-		printf("%d", ((1 << offset--) & *c) ? 1 : 0);
-	}
+  int offset = 7;
+  while (offset >= 0)
+    {
+      printf ("%d", ((1 << offset--) & *c) ? 1 : 0);
+    }
 }
 
+ssize_t
+read_socket_data (int cli_socket_fd, void **buf, size_t * buf_len)
+{
+  int pkg_capacity;
+  int bs_count;			// count of bytes read at a time
+  int buf_cursor = 0;		// It also indicates the total number of bytes that have been read
+  int remaining_retry = 10;	// Retry attempts remaining
+
+  do
+    {
+      bs_count =
+	read (cli_socket_fd, ((void *) &pkg_capacity) + buf_cursor,
+	      sizeof (pkg_capacity) - buf_cursor);
+
+      if (bs_count < 0)
+	return -1;
+
+      if (bs_count == 0 && remaining_retry-- == 0)
+	return -1;
+
+      buf_cursor += bs_count;
+    }
+  while (buf_cursor < sizeof (pkg_capacity));
+
+  if (pkg_capacity <= sizeof (pkg_capacity))
+    return -1;
+
+  char *m_buf = malloc (pkg_capacity);	// memory buffer must be freed elsewhere!
+
+  memcpy (m_buf, &pkg_capacity, sizeof (pkg_capacity));
+
+  while (buf_cursor < pkg_capacity)
+    {
+      bs_count =
+	read (cli_socket_fd, m_buf + buf_cursor, pkg_capacity - buf_cursor);
+      buf_cursor += bs_count;
+    }
+
+  *buf = m_buf;
+  return *buf_len = pkg_capacity;
+}
--- src/utils.h
+++ src/utils.h
@@ -1,7 +1,8 @@
 #ifndef _EUCLID_OAE__UTILS_H_
 #define _EUCLID_OAE__UTILS_H_ 1
 
-void print_byte_binary(char *c);
+void print_byte_binary (char *c);
+
+ssize_t read_socket_data (int cli_socket_fd, void **buf, size_t * buf_len);
 
 #endif
-
