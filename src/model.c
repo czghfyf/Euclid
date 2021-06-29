69dd48cd7e0f6ba5fbc5ac0a5cc96f77e6c66a8e
czg <hellochenzg@163.com>
2021-06-29 17:03:17 +0800

    ^ save dimension info into file

--- src/model.c
+++ src/model.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <string.h>
-// 
+
 // #include "cfg.h"
 #include "model.h"
 #include "utils.h"
@@ -16,10 +16,18 @@ Dimension *create_dimension(char *name)
 	int name_len =
 	    strlen(name) < DIM_STCT_NAME_LEN ? strlen(name) : DIM_STCT_NAME_LEN;
 
-	// global_id gid;
 	gen_global_id(&(dim->gid));
-	printf("****************************************************** %lld\n",
-	       dim->gid);
+	printf("********************* %lld\n", dim->gid);
+
+	char dim_file[256];
+	memset(dim_file, 0, 256);
+	getcwd(dim_file, 220);
+	strcat(dim_file, "/meta/dimensions");
+	printf("dimensions file absolute path: %s\n", dim_file);
+
+	FILE *fp = fopen(dim_file, "a+");
+	fwrite(dim, sizeof(Dimension), 1, fp);
+	fclose(fp);
 
 	return dim;
 }
